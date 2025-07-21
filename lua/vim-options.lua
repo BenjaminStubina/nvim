95e9fe4abe64540f65eaf80df936d4212159341b
BenjaminStubina <benjamin.s@roserocket.com>
2025-07-21 19:10:07 -0400

    add harpoon and conform plugins, modify lsp to handle ts diagnostics

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,9 +3,15 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.g.mapleader = " "
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.wo.number = true
+
+vim.diagnostic.config({ virtual_lines = false })
+vim.diagnostic.config({ virtual_text = true })
 
 -- Navigate vim panes better
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
