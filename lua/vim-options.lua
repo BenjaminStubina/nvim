fca9e162d580889b85b08509c7af0100a1a78594
Benjamin Stubina <benjamin.stubina@gmail.com>
2025-10-08 01:13:03 -0400

    Add precognition and smear plugins + add some key maps

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -11,7 +11,29 @@ vim.diagnostic.config({ virtual_lines = false })
 vim.diagnostic.config({ virtual_text = true })
 
 -- Navigate vim panes better
-vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
-vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
 vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
 vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
+
+-- Move highlighted code blocks
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Append line below to current line and keep cursor in place
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- Jump half page and center cursor
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Yank paste without reseting paste buffer
+vim.keymap.set("x", "<leader>p", '"_dP')
+
+-- Yank into +register
+vim.keymap.set("n", "<leader>y", '"+y')
+vim.keymap.set("v", "<leader>y", '"+y')
+vim.keymap.set("n", "<leader>Y", '"+Y')
+
+-- Replace all instances of selected word
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
